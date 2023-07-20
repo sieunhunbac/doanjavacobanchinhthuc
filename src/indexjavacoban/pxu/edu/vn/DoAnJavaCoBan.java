e213f3850b8f90d7771b1123148e125c683f987e
Hanabi <Hanabi@LAPTOP-37NF4L06>
2023-07-20 13:22:48 +0700

    thay doi lan 2

--- src/indexjavacoban/pxu/edu/vn/DoAnJavaCoBan.java
+++ src/indexjavacoban/pxu/edu/vn/DoAnJavaCoBan.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class DoAnJavaCoBan {
 
 	public static void main(String[] args) {
+		inMenu();
 		int n = nhapSoLuongSanPham();
 		String[] masanpham = new String[n];
 		String[] tensanpham = new String[n];
@@ -13,10 +14,25 @@ public class DoAnJavaCoBan {
 		double[] giaban = new double[n];
 		double[] thanhtien = new double[n];
 		nhapThongTinSanPham(masanpham, tensanpham, donvitinh, soluong, giaban);
+		thanhtien = tinhThanhTien(soluong, giaban);
 		inDanhSachSanPham(masanpham, tensanpham, donvitinh, soluong, giaban, thanhtien);
+		sapxepThanhTienGiamDan(masanpham, tensanpham, donvitinh, soluong, giaban, thanhtien);
+		sapxepGiaBanTangDan(masanpham, tensanpham, donvitinh, soluong, giaban, thanhtien);
+		timSanPhamTheoTen(masanpham, tensanpham, donvitinh, soluong, giaban, thanhtien);
 	}
-
-	// nhap so luong san pham
+	//HÀM MENU CHÍNH
+	public static void inMenu() {
+		System.out.println("----------------------------------MENU QUẢN LÝ SẢN PHẨM --------------------------------");
+		System.out.println("|(1): Nhập thông tin sản phẩm                                                          |");
+		System.out.println("|(2): Hiển thị danh sách sản phẩm                                                      |");
+		System.out.println("|(3): Tính THÀNH TIỀN sản phẩm                                                         |");
+		System.out.println("|(4): Sắp xếp Thành Tiền giảm dần                                                      |");
+		System.out.println("|(5): Sắp xếp Gia Bán tăng dần                                                         |");
+		System.out.println("|(6): Tìm sản phẩm Theo Tên                                                            |");
+		System.out.println("|(Q/q): Thoát khỏi chương trình                                                        |");
+		System.out.println("----------------------------------------------------------------------------------------");
+	}
+	// NHẬP SỐ LƯỢNG SẢN PHẨM
 	public static int nhapSoLuongSanPham() {
 		try {
 			int n;
@@ -32,10 +48,10 @@ public class DoAnJavaCoBan {
 		}
 	}
 
-	// NHẬP THÔNG TIN SẢN PHẨM
+	// HÀM NHẬP THÔNG TIN SẢN PHẨM
 	public static void nhapThongTinSanPham(String[] masanpham, String[] tensanpham, String[] donvitinh,
 			double[] soluong, double[] giaban) {
-		System.out.println("*************************************************************");
+		System.out.println("***************************************************************************************");
 		try {
 			for (int i = 0; i < masanpham.length; i++) {
 				Scanner sc = new Scanner(System.in);
@@ -54,7 +70,7 @@ public class DoAnJavaCoBan {
 		} catch (Exception e) {
 			System.out.println("DA XAY RA LOI TRONG QUA TRINH NHAP DU LIEU");
 		}
-		System.out.println("*************************************************************");
+		System.out.println("***************************************************************************************");
 	}
 
 	// HÀM TÍNH THÀNH TIỀN
@@ -63,7 +79,7 @@ public class DoAnJavaCoBan {
 		for (int i = 0; i < soluong.length; i++) {
 			thanhtien[i] = Math.round((soluong[i] * giaban[i]) * 1000.0) / 1000.0;
 		}
-		System.out.println("+++++++++++++++++++++DA TINH THANH TIEN ++++++++++++++++++++");
+		System.out.println("++++++++++++++++++++++++++++++++ ĐÃ TÍNH THÀNH TIỀN ++++++++++++++++++++++++++++++++");
 		return thanhtien;
 	}
 
@@ -75,12 +91,116 @@ public class DoAnJavaCoBan {
 		System.out.println("\t\t\t\t\t |");
 		System.out.println("\t\t\t\t\t V");
 		System.out.println("\t\t\t\t DANH SÁCH SẢN PHẨM");
-		System.out.println("----------------------------------------------------------------------------");
-		System.out.printf("%-13s%-14s%-13s%-11s%-9s%-15s\n","|Mã sản phẩm |","Tên sản phẩm |","Đơn vị tính |","Số lượng |","Gía bán |","Thành tiền VNĐ|");
+		System.out.println("----------------------------------------------------------------------------------------");
+		System.out.printf("%-6s%-13s%-14s%-13s%-11s%-9s%-15s\n","| STT ","| Mã sản phẩm |"," Tên sản phẩm |"," Đơn vị tính |"," Số lượng |"," Giá bán |"," Thành tiền VNĐ |");
 		for (int i = 0; i < masanpham.length; i++) 
-		{ 	System.out.println("|------------|-------------|------------|---------|---------|--------------|");
-			System.out.printf("%-13.13s%-14.13s%-13.13s%-10s%-10s%-15s%-15s\n","|"+masanpham[i],"|"+tensanpham[i],"|"+donvitinh[i],"|"+soluong[i],"|"+giaban[i],"|"+thanhtien[i],"|");}
-		System.out.println("----------------------------------------------------------------------------");
+		{ 	System.out.println("|-----|-------------|--------------|-------------|----------|---------|----------------|");
+			System.out.printf("%-6s%-14.14s%-15.15s%-14.14s%-11s%-10s%-16s%-15s\n","|  "+i,"| "+masanpham[i],"| "+tensanpham[i],"| "+donvitinh[i],"| "+soluong[i],"| "+giaban[i],"| "+thanhtien[i]+"VNĐ"," |");}
+		System.out.println("----------------------------------------------------------------------------------------");
 	}
+	//HÀM SẮP XẾP THEO CHIỀU GIẢM DẦN CỦA THÀNH TIỀN
+	public static void sapxepThanhTienGiamDan(String[] masanpham, String[] tensanpham, String[] donvitinh,double[] soluong, double[] giaban,double[] thanhtien) {
+	for (int i = 0; i < thanhtien.length - 1; i++) {
+		for (int j = i + 1; j < thanhtien.length; j++) {
+			if (thanhtien[i] < thanhtien[j]) {
+				// HOÁN VỊ THÀNH TIỀN
+				double tam = thanhtien[i];
+				thanhtien[i] =thanhtien[j];
+				thanhtien[j] = tam;
+				// HOÁN VỊ SỐ LƯỢNG
+				tam = soluong[i];
+				soluong[i] =soluong[j];
+				soluong[j] = tam;
+				// HOÁN VỊ GIÁ BÁN
+				tam = giaban[i];
+				giaban[i] =giaban[j];
+				giaban[j] = tam;
+				// HOÁN VỊ MÃ SẢN PHẨM
+				String tam1 = masanpham[i];
+				masanpham[i] = masanpham[j];
+				masanpham[j] = tam1;
+				// HOÁN VỊ TỂN SẢN PHẨM
+				tam1 = tensanpham[i];
+				tensanpham[i] = tensanpham[j];
+				tensanpham[j] = tam1;
+				// HOÁN VỊ ĐƠN VỊ TÍNH
+				tam1 = donvitinh[i];
+				donvitinh[i] = donvitinh[j];
+				donvitinh[j] = tam1;
+			}
+		}
+	}
+	  System.out.println("+++++++++++++++++++++++++ ĐÃ SẮP XẾP THEO THÀNH TIỀN GIẢM DẦN +++++++++++++++++++++++++");
+	inDanhSachSanPham(masanpham, tensanpham, donvitinh, soluong, giaban, thanhtien);
+}
+
+// HÀM SẮP XẾP THEO CHIỀU GIẢM TĂNG CỦA GIÁ BÁN
+public static void sapxepGiaBanTangDan(String[] masanpham, String[] tensanpham, String[] donvitinh, double[] soluong,
+		double[] giaban, double[] thanhtien) {
+	for (int i = 0; i < giaban.length - 1; i++) {
+		for (int j = i + 1; j < giaban.length; j++) {
+			if (giaban[i] > giaban[j]) {
+				// HOÁN VỊ GIÁ BÁN
+				double tam = giaban[i];
+				giaban[i] = giaban[j];
+				giaban[j] = tam;
+				// HOÁN VỊ SỐ LƯỢNG
+				tam = soluong[i];
+				soluong[i] = soluong[j];
+				soluong[j] = tam;
+				// HOÁN VỊ THÀNH TIỀN
+				tam = thanhtien[i];
+				thanhtien[i] = thanhtien[j];
+				thanhtien[j] = tam;
+				// HOÁN VỊ MÃ SẢN PHẨM
+				String tam1 = masanpham[i];
+				masanpham[i] = masanpham[j];
+				masanpham[j] = tam1;
+				// HOÁN VỊ TỂN SẢN PHẨM
+				tam1 = tensanpham[i];
+				tensanpham[i] = tensanpham[j];
+				tensanpham[j] = tam1;
+				// HOÁN VỊ ĐƠN VỊ TÍNH
+				tam1 = donvitinh[i];
+				donvitinh[i] = donvitinh[j];
+				donvitinh[j] = tam1;
+			}
+		}
+	}
+	System.out.println("+++++++++++++++++++++++++++ ĐÃ SẮP XẾP THEO GIÁ BÁN TĂNG DẦN +++++++++++++++++++++++++++");
+	inDanhSachSanPham(masanpham, tensanpham, donvitinh, soluong, giaban, thanhtien);
+}
+
+	// HÀM TÌM KIẾM THEO TÊN SẢN PHẨM
+	public static void timSanPhamTheoTen(String[] masanpham, String[] tensanpham, String[] donvitinh, double[] soluong,
+			double[] giaban, double[] thanhtien) {
+		int count = 0;
+		Scanner sc = new Scanner(System.in);
+		String x;
+		try {
+			// do {
+			System.out.print("-----------> Nhập tên sản phẩm cần tìm : ");
+			x = sc.nextLine();
+			// }while();
+
+			for (int i = 0; i < tensanpham.length; i++) {
+				if (tensanpham[i].equals(x)) {
+					System.out.println("----------------------------------------------------------------------------------------");
+					System.out.printf("%-6s%-13s%-14s%-13s%-11s%-9s%-15s\n","| STT ","| Mã sản phẩm |"," Tên sản phẩm |"," Đơn vị tính |"," Số lượng |"," Giá bán |"," Thành tiền VNĐ |");
+					 	System.out.println("|-----|-------------|--------------|-------------|----------|---------|----------------|");
+						System.out.printf("%-6s%-14.14s%-15.15s%-14.14s%-11s%-10s%-16s%-15s\n","|  "+i,"| "+masanpham[i],"| "+tensanpham[i],"| "+donvitinh[i],"| "+soluong[i],"| "+giaban[i],"| "+thanhtien[i]+"VNĐ"," |");
+					System.out.println("----------------------------------------------------------------------------------------");
+					count = count + 1;
+				}
+			}
+	System.out.println("================================= ĐÃ TÌM THẤY SẢN PHẨM  ================================");
+			if (count == 0) {
+				System.out.println("KHÔNG TÌM THẤY");
+			}
+		} catch (Exception e) {
+			System.out.println("!!! ĐÃ XẢY RA LỖI TRONG QUÁ TRÌNH NHẬP DỮ LIỆU !!!");
+		}
+	}
+
 	}
 
